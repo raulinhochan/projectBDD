f683a130a7c2e343bc59d0bcab7dc7e94fa14f4c
raulinhochan <raul.alegria.blanco@gmail.com>
2018-12-11 22:17:47 +0100

    Initial commit

---
+++ src/test/java/primerTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class primerTest {
+    @Test
+    public void primera(){
+        Assert.assertEquals("OK, OLE", "OLE","OLE");
+    }
+
+}
